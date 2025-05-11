fe230af257f891096516002e8aab49a00eb73880
“GeorgeGitSter” <chetsada142546@gmail.com>
2025-05-11 13:52:09 +0700

    MAIN: Welcome, Login, Signup UI

---
+++ lib/views/welcome_ui.dart
@@ -0,0 +1,94 @@
+// ignore_for_file: sort_child_properties_last
+
+import 'package:flutter/material.dart';
+
+class WelcomeUI extends StatelessWidget {
+  const WelcomeUI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 120,
+            ),
+            Image.asset(
+              'assets/images/img.png',
+              width: 350,
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Text(
+              'WELCOME TO DTI',
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              'Let\'s Get Started :)',
+            ),
+            Text(
+              'By GeorgeSter',
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                      fixedSize: Size(
+                        130,
+                        40,
+                      ),
+                      side: BorderSide(
+                        color: Colors.black, // เปลี่ยนสีขอบเป็นสีดำ
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          10,
+                        ),
+                      )),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'SIGNUP',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: Size(130, 40),
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          10,
+                        ),
+                      )),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
